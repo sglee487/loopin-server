c9c2a53f2a79fc0c8fc4e92c5f37a95bdd9b1328
sanggeon lee <sglee487@gmail.com>
2024-05-11 17:52:28 +0900

    get video file with bytes

--- src/main/kotlin/site/sg/snserver_spring/video/FileViewController.kt
+++ src/main/kotlin/site/sg/snserver_spring/video/FileViewController.kt
@@ -1,7 +1,17 @@
 package site.sg.snserver_spring.video
 
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.MediaType
+import org.springframework.util.FileCopyUtils
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class FileViewController {
+    @GetMapping("/video", produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
+    fun getVideoFileBytes(): ByteArrayResource {
+        val resource = ClassLoader.getSystemResource("data/test1.mp4") // Use ClassLoader directly
+        return ByteArrayResource(FileCopyUtils.copyToByteArray(resource.openStream()))
+    }
+
 }
