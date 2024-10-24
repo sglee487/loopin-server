71c2fded67ea84cbd13a0526951efbac5d13fc78
sglee487 <sglee487@gmail.com>
2024-10-24 22:00:09 +0900

    use mongodb only

--- src/main/kotlin/site/sg/snserver_spring/SserverSpringApplication.kt
+++ src/main/kotlin/site/sg/snserver_spring/SserverSpringApplication.kt
@@ -1,9 +1,10 @@
 package site.sg.snserver_spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class SnServerSpringApplication
 
 fun main(args: Array<String>) {
