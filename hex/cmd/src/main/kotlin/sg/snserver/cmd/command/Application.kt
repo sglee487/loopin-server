a5edd9f463be4df15ea97d951f7ba78e90911014
sglee <sglee487@gmail.com>
2025-01-15 19:23:59 +0900

    chore: unused import

--- hex/cmd/src/main/kotlin/sg/snserver/cmd/command/Application.kt
+++ hex/cmd/src/main/kotlin/sg/snserver/cmd/command/Application.kt
@@ -2,7 +2,6 @@ package sg.snserver.cmd.command
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
