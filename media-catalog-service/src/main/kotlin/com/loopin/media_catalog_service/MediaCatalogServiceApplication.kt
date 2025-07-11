03970061392aafebe6140e5429555ba8ef9b26de
sg <sglee487@gmail.com>
2025-07-12 01:37:18 +0900

    Refactor application setup by removing unused annotations
    Removed `@EnableCaching` and `@EnableR2dbcAuditing` as they were not being utilized in the application. This simplifies the configuration and reduces unnecessary dependencies. Indentation for the `runApplication` call was also adjusted for consistency.

--- media-catalog-service/src/main/kotlin/com/loopin/media_catalog_service/MediaCatalogServiceApplication.kt
+++ media-catalog-service/src/main/kotlin/com/loopin/media_catalog_service/MediaCatalogServiceApplication.kt
@@ -3,13 +3,11 @@ package com.loopin.media_catalog_service
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class MediaCatalogServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<MediaCatalogServiceApplication>(*args)
+    runApplication<MediaCatalogServiceApplication>(*args)
 }
